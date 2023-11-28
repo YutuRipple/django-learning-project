a3812dc8d9b75f14265c56c76ecde23de4b9842d
Yutu Ripple <ericripple617@gmail.com>
2023-11-28 10:08:57 +0000

    create game app

--- game/views.py
+++ game/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+def index(request):
+    line1 = '<h1 style="text-align:center;">涟漪今天吃什么</h1>'
+    return HttpResponse(line1)
