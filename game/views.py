d232239d7b6fe8fe53fda6f9219cc7d5a9f0a8d1
Yutu Ripple <ericripple617@gmail.com>
2023-11-30 07:44:26 +0000

    update game view

--- game/views.py
+++ game/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
 def index(request):
-    line1 = '<h1 style="text-align:center;">涟漪今天吃什么</h1>'
-    return HttpResponse(line1)
+    line1 = '<h1 style="text-align:center;">Backrooms Ordeal</h1>'
+    line2 = '<img width="100%" src="https://pbs.twimg.com/media/FROzUkdaIAAU0P4?format=jpg&name=4096x4096"></img>'
+    return HttpResponse(line1+line2)
