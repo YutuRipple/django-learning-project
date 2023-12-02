ec6ab4383593df0acacd95fecd150436e6a72242
Yutu Ripple <ericripple617@gmail.com>
2023-12-02 03:23:02 +0000

    optimized project structure

--- game/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-def index(request):
-    line1 = '<h1 style="text-align:center;">Backrooms Ordeal</h1>'
-    line2 = '<img width="100%" src="https://pbs.twimg.com/media/FROzUkdaIAAU0P4?format=jpg&name=4096x4096"></img>'
-    return HttpResponse(line1+line2)
