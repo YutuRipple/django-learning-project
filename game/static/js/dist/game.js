d5b242590ff609dbf30fd4f865d2311a71f8f1ff
Yutu Ripple <ericripple617@gmail.com>
2023-12-07 13:29:09 +0000

    create game player model

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -119,6 +119,34 @@ class GameMap extends AcGameObject{
         this.ctx.fillRect(0,0,this.ctx.canvas.width,this.ctx.canvas.height);
     }
 }
+class Player extends AcGameObject{
+    constructor(playground,x,y,radius,color,speed,is_me){
+          super();
+          this.playground=playground;
+          this.ctx=this.playground.game_map.ctx;
+          this.x=x;
+          this.y=y;
+          this.radius=radius;
+          this.color=color;
+          this.speed = speed;
+          this.is_me=is_me;
+          this.eps=0.1;
+    }
+
+    start(){
+    }
+
+    update(){
+        this.render();
+    }
+
+    render(){
+        this.ctx.beginPath();
+        this.ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
+        this.ctx.fillStyle=this.color;
+        this.ctx.fill();
+    }
+}
 class AcGamePlayground{
     constructor(root){
         this.root=root;
@@ -130,6 +158,8 @@ class AcGamePlayground{
         this.width=this.$playground.width();
         this.height=this.$playground.height();
         this.game_map=new GameMap(this);
+        this.players = [];
+        this.players.push(new Player(this,this.width/2,this.height/2,this.height*0.05,"white",this.height*0.15,true))
         this.start();
     }
 
